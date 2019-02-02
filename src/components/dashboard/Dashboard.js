80c2aa024dfca3dc89c9c4abb1c48195b8b1b2f0
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-02-02 12:33:16 -0500

    Created dashboard

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the dashbaord</p>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
