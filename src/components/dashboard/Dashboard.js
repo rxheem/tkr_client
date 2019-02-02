c46f35c549c5fe991347e5d5f4c8b67a6f4a5342
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-02-02 12:34:40 -0500

    Updated dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+// Including all the other components that will be displayed here
+
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <p>This is the dashbaord</p>
+        <p>This is the dashbaord. Its not 100% yet, so give it some time! :)</p>
       </div>
     );
   }
