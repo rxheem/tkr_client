af7cc804fcde12ae3ad618800bf11a097c463265
Raheem McDonald <37784173+rxheem@users.noreply.github.com>
2019-02-03 11:21:52 -0500

    Update Header.js
    Added improved documentation. The Header is no longer animated so I removed the animation import. Also, this is a functional component and the Container from semantic-ui-react and the { Component } from React were not being used so they they they were removed.

--- src/views/header/Header.js
+++ src/views/header/Header.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
-import { Container } from "semantic-ui-react";
-import { Animated } from "react-animated-css";
+/*  
+*   The Header serves as a constant navigation that will be displayed throughout the application, 
+*   regardless of which screen/ page the user navigates to. Only on link is working, the "Sign In"
+*   which redirects the user to the Sign In page if te member has nto yet been authenticated. 
+*/
+
+import React from "react";
 import { NavLink } from "react-router-dom";
 
+// Importing the CSS file here for styling
 import "./Header.css";
 
 const Header = () => {
@@ -19,9 +24,11 @@ const Header = () => {
             <i className="search link icon" />
           </div>
         </div>
+    
         <NavLink className="ui item" to="/signin">
           Sign in
         </NavLink>
+    
       </div>
     </div>
   );
